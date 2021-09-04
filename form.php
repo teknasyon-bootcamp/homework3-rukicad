fbc573ee50982d349ba408f2c01ce0e041fa39aa
Rukiye Çadırcı <83542142+rukicad@users.noreply.github.com>
2021-09-04 09:41:06 +0300

    ödev.php
    "Ödev sisteme yüklenmiştir."

--- form.php
+++ form.php
@@ -1,26 +1,59 @@
+  
 <?php
+sınıf  Formu
+{    
+    var string $ eylem ;    //"var" anahtarlar, bir sınıflarını ya da gecerken kamusal iletilecek
+    var string $ method ;
+    var dizi $ alanları =[];
+    
+    özel  işlev  __construct ( $ eylem , $ yöntem )
+    {
+        $ bu -> işlem = $ işlem ; //burada const'ile nesnemizi ilk uygulama hazırlıyoruz
+        $ bu -> yöntem = $ yöntem ; //ve burada da ..
+    }
 
-/**
- * UML diyagramında yer alan Form sınıfını oluşturmanız beklenmekte.
- * 
- * Sınıf içerisinde static olmayan `fields`, `action` ve `method`
- * özellikleri (property) olması gerekiyor.
- * 
- * Sınıf içerisinde static olan ve Form nesnesi döndüren `createPostForm`,
- * `createGetForm` ve `createForm` methodları bulunmalı. Bu metodlar isminde de
- * belirtilen metodlarda Form nesneleri oluşturmalı.
- * 
- * Sınıf içerisinde bir "private" başlatıcı (constructor) bulunmalı. Bu başlatıcı
- * içerisinden `action` ve `method` değerleri alınıp ilgili property'lere değerleri
- * aktarılmalıdır.
- * 
- * Sınıf içerisinde static "olmayan" aşağıdaki metodlar bulunmalıdır.
- *   - `addField` metodu `fields` property dizisine değer eklemelidir.
- *   - `setMethod` metodu `method` propertysinin değerini değiştirmelidir.
- *   - `render` metodu form'un ilgili alanlarını HTML çıktı olarak verip bir buton çıktıya eklemelidir.
- * 
- * Sonuç ekran görüntüsüne `result.png` dosyasından veya `result.html` dosyasından ulaşabilirsiniz.
- * `app.php` çalıştırıldığında `result.html` ile aynı çıktıyı verecek şekilde geliştirme yapmalısınız.
- * 
- * > **Not: İsteyenler `app2.php` ve `form2.php` isminde dosyalar oluşturup sınıfa farklı özellikler kazandırabilir.
- */
+
+    genel  statik  işlev  createGetForm ( $ eylem ) 
+    {
+         kendi kendine dön :: createForm ( $ eylem , "GET" );    //const(sabit) ve staticlerde self:: kullanılır, uyarlamalarda this->
+    }
+    genel  statik  işlev  createForm ( $ eylem , $ yöntem )
+    {
+        return  yeni  Formu ( $ eylem , $ yöntemi );           //const(sabit) ve staticlerde self:: kullanılır, uyarlamalarda this->
+    }
+    genel  statik  işlev  createPostForm ( $ action )  
+    {
+         kendi kendine dön :: createForm ( $ eylem , "POST" );   //const(sabit) ve staticlerde self:: kullanılır, uyarlamalarda this->
+    }
+
+
+
+    public  function  addField ( string  $ title , string  $ value , ? string  $ defaultValue = null ) //addField için ileri (başlık,değer ve varsa default atanmış değer)->app.php 14.satır
+    { 
+        $ alanlar =[ $ başlık , $ değer , $ defaultValue ];   //fields[] 'bir değer ilavesi
+
+        $ this -> alanlar []= $ alanlar ;
+    }
+
+    public  function  setMethod ( $ method )   //method set edildi
+    {
+        $ bu -> yöntem = $ yöntem ;  
+    }
+
+    public  function  render ()   //burada büyütülecek html ile ilgili gerekli işlemlerle birlikte
+    { 
+
+        foreach ( $ this -> $ değeri olarak alanlar  ) { 
+            echo  "<form yöntemi='" . $ this -> yöntemi . "' eylem ='" . $ bu -> eylem . "'>\n" ;
+
+           echo  " <label for='" . $ değeri [ 1 ] . "'>" . $ değeri [ 0 ] . "</label>\n" ;
+            if ( isset ( $ değer [ 2 ])){    //bu 'if' age ve türevleri için
+                echo  " <input type='text' name='" . $ değeri [ 1 ] . "' yer tutucu ='" . $ değeri [ 2 ]. "' />\n" ;
+            }
+            başka {
+                echo  " <input type='text' name='" . $ değeri [ 1 ] . "' placeholder='Değeri girin' />\n" ;
+            }
+        }
+        echo  " <button type='submit'>Gönder</button>\n</form>\n" ;
+    }
+}
